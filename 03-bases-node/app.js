a54b294f630fe182412fd00d30696484140f089e
vihus <vi.hu.sc@gmail.com>
2021-11-06 17:10:12 -0600

    reacomodo de package

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,9 @@
 const { crearArchivo } = require('./crearAcrchivo');
 const argv = require('yargs').argv;
 
+console.log(process.argv);
+console.log(argv);
+
 crearArchivo(3)
     .then(n_archivo => console.log(`Archivo ${n_archivo} creado exitosamente`))
     .catch(error => console.error(error));
